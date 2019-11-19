7ad95fc3263cee52983699647f2ebc258a26e3dd
Ashish Verma <54759496+ashishv8097@users.noreply.github.com>
2019-11-19 11:30:46 -0500

    Add files via upload

---
+++ sys/xdone.c
@@ -0,0 +1,14 @@
+/* xdone.c - xdone */
+#include <kernel.h>
+#include <stdio.h>
+
+/*------------------------------------------------------------------------
+ * xdone  --  print system completion message as last process exits
+ *------------------------------------------------------------------------
+ */
+int xdone()
+{
+    	kprintf("\n\nAll user processes have completed.\n\n");
+	shutdown();
+	return(OK);
+}
